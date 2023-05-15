169d3371bfcb589bf283620323952e0aa08632a4
billgeiz <billgeiz@freenet.de>
2023-05-16 01:19:02 +0200

    Base for Main Class

--- src/parkPack/Main.java
+++ src/parkPack/Main.java
@@ -1,7 +1,17 @@
 package parkPack;
 
-class Main{
-	public static void main(String[]args){
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		int input;
+		Scanner myScanner = new Scanner(System.in);
+		Parkhaus myParkhaus = new Parkhaus();
 		System.out.println("PARKHAUS");
+		input = myScanner.nextInt();
+
+		do {
+
+		} while (input != 0);
 	}
 }
